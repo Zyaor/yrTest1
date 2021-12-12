a0b870f6e5685b35b56408864805d1fa4cb2c1d4
Zyaor <zguor033@163.com>
2021-12-12 21:26:25 +0800

    1

--- TestMaven/src/main/java/com/yr/hello.java
+++ TestMaven/src/main/java/com/yr/hello.java
@@ -1,7 +1,40 @@
-package com.yr;
+package java.com.yr;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class hello {
     public static void main(String[] args) {
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+           5,10,1L,
+                TimeUnit.SECONDS,new ArrayBlockingQueue<>(100),
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        ) ;
+
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println("线程名:"+i);
+            int finalI = i;
+            executor.execute(
+                () ->{
+                    try {
+                        Thread.sleep(100);
+                        System.out.println("第"+ finalI +"个线程，线程名："+Thread.currentThread().getName());
+                    }catch (Exception e){
+
+                    }
+                }
+            );
+        }
+        executor.shutdown();
+        try {
+            executor.awaitTermination(5,TimeUnit.SECONDS);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
         System.out.println("测试分支");
     }
 }
